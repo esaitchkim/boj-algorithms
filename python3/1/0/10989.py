2696631495e5a476bb44789b95a3fbdfc762e9d5
esaitchkim <esaitchkim@gmail.com>
2024-08-13 20:11:38 +0900

    feat: 문제 해결
    해결 방안 추가

--- python3/1/0/10989.py
+++ python3/1/0/10989.py
@@ -3,6 +3,28 @@ Title: 수 정렬하기 3
 Url: https://www.acmicpc.net/problem/10989
 """
 
+# case 1
+# import sys
+
+# input = sys.stdin.readline
+# print = sys.stdout.write
+
+# N = int(input())
+
+# number_list = [0 for _ in range(10001)]
+# total_cnt = 0
+# cnt = 0
+# for _ in range(N):
+#     number = int(input())
+#     number_list[number] += 1
+
+# for i in range(10001):
+#     if number_list[i] > 0:
+#         for j in range(number_list[i]):
+#             print(f"{i}\n")
+
+# case 2
+# 속도 조금 개선
 import sys
 
 input = sys.stdin.readline
@@ -19,5 +41,11 @@ for _ in range(N):
 
 for i in range(10001):
     if number_list[i] > 0:
-        for j in range(number_list[i]):
-            print(f"{i}\n")
+        while number_list[i] > 0:
+            cnt = min(100000, number_list[i])
+            number_list[i] -= cnt
+            total_cnt += cnt
+            print(f"{i}\n" * cnt)
+
+    if total_cnt == N:
+        break
